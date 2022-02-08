7d34bd05daf189df630113b5e20b9c151ff2a78a
Anthony Babka <anthonybabka@gmail.com>
2022-02-08 14:19:05 -0800

    fixed map errors

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+window.onload = function(){
+   setTimeout(loadAfterTime, 1000)
+};
+
+function loadAfterTime() { 
+var map = L.map('map').setView([37.644224, 140.372278], 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -7,6 +12,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoiYWJhYmthIiwiYSI6ImNremVudGI0YTNhYWEybnBoeWV1bm1wcmIifQ.wRTTMF4BgP9Q8AhZldWojA'
-}).addTo(map);
+}).addTo(map); 
+}
 
-// console error - map container not found, cant find api documentation =(
+// need to work on finding users location and appearing on map instead of Ebisu Circuit
