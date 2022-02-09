27e6d75028d327fcc1e65e326aa43cb568a9ebf4
Anthony Babka <anthonybabka@gmail.com>
2022-02-08 20:32:30 -0800

    updated map to user location

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-window.navigator.geolocation
-  .getCurrentPosition(console.log, console.log);
-
 window.onload = function(){
    setTimeout(loadAfterTime, 1000)
 };
 
-function loadAfterTime() { 
-var map = L.map('map').setView([37.644224, 140.372278], 13);
+function loadAfterTime() {
+
+var map = L.map('map').fitWorld();
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -15,6 +13,21 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoiYWJhYmthIiwiYSI6ImNremVudGI0YTNhYWEybnBoeWV1bm1wcmIifQ.wRTTMF4BgP9Q8AhZldWojA'
-}).addTo(map); 
+}).addTo(map);
+
+map.locate({setView: true, maxZoom: 15});
+
+function onLocationFound(e) {
+  var radius = e.accuracy;
+
+  L.marker(e.latlng).addTo(map)
+      .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+  L.circle(e.latlng, radius).addTo(map);
 }
-// need to work on finding users location and appearing on map instead of Ebisu Circuit
+
+map.on('locationfound', onLocationFound);
+
+}
+
+// need to work on locating businesses
